3d0c41c083d8c551734b23812591e550e99d3e01
huajieyang <huajieyang@dbs.com>
2017-03-01 17:48:53 +0800

    3 - multiple components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
 
+import {Hero} from './hero'
+
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+
 @Component({
   selector: 'my-app',
   // template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
@@ -12,15 +29,31 @@ import { Component } from '@angular/core';
   <input [(ngModel)]="hero.name" placeholder="name">
   `*/
 
-  template: `
+ /* template: `
+    <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes"
+          [class.selected]="hero === selectedHero"
+          (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-  `,
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `,*/
 
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `,
 
   styles: [`
     .selected {
@@ -82,30 +115,17 @@ export class AppComponent  {
   };*/
 
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
 
 
-export class Hero {
-  id: number;
-  name: string;
-}
 
 
 
 
-
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 
---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by huajieyang on 01/03/17.
+ */
+
+export class Hero {
+  id: number;
+  name: string;
+}
